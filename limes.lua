ee4e475d3808e80171d790dcb4046b542e605c10
Neo <mario.sieg.64@gmail.com>
2024-05-07 13:23:51 +0200

    Update limes.lua

--- limes.lua
+++ limes.lua
@@ -1,6 +1,6 @@
-function limes(f, a, eps) -- Evaluate the limit of f(x) as x approaches a from both sides with a given epsilon
-    local x1 = a - eps
-    local x2 = a + eps
+function limes(f, a, ε) -- Evaluate the limit of f(x) as x approaches a from both sides with a given epsilon
+    local x1 = a - ε
+    local x2 = a + ε
     local f1 = f(x1)
     local f2 = f(x2)
     local limit = (f1 + f2) / 2
@@ -9,7 +9,7 @@ end
 
 -- evaluate for f(x) = x^2
 local f = function(x) return x^2 end
-local eps = 0.1
+local ε = 0.1
 for x = 1, 10 do
-    print(string.format('lim(%f) = %f', x, limes(f, x, eps)))
+    print(string.format('lim(%f) = %f', x, limes(f, x, ε)))
 end
